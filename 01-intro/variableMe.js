47ccfa5df10e24555431f105eb0689f58357f78f
shreyas bole <shreyasbole4@gmail.com>
2023-08-17 00:15:26 +0530

    variables started

---
+++ 01-intro/variableMe.js
@@ -0,0 +1,2 @@
+var greetings = "Hello Shreyas";
+console.log(greetings);
---
+++ 01-intro/variableMe.ts
@@ -0,0 +1,9 @@
+let greetings: string = "Hello Shreyas";
+
+let myNum = 10;
+
+// myNum.toUpperCase();
+
+console.log(greetings);
+
+export {};
