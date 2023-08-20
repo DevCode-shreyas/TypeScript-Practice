ce5767a383b5a1de0b24f9236fc389cb0fb6acb2
shreyas bole <shreyasbole4@gmail.com>
2023-08-21 00:28:37 +0530

    any

--- 01-intro/variableMe.js
+++ 01-intro/variableMe.js
@@ -1,2 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Shreyas";
-console.log(greetings);
+// myNum.toUpperCase();
+console.log(greetings); // Hello Shreyas
+// number type
+var userId = 2233;
+// boolean type
+var isLoggedIn = false;
+isLoggedIn.valueOf();
+// any type
+// desc: any type is used to store any type of value
+var hero;
+function getHero() {
+    return "Batman";
+}
+hero = getHero();
--- 01-intro/variableMe.ts
+++ 01-intro/variableMe.ts
@@ -1,9 +1,29 @@
 let greetings: string = "Hello Shreyas";
 
-let myNum = 10;
-
 // myNum.toUpperCase();
 
 console.log(greetings); // Hello Shreyas
 
 export {};
+
+// number type
+
+let userId = 2233;
+
+// boolean type
+
+let isLoggedIn: boolean = false;
+
+isLoggedIn.valueOf();
+
+// any type
+
+// desc: any type is used to store any type of value
+
+let hero: string;
+
+function getHero() {
+  return "Batman";
+}
+
+hero = getHero();
